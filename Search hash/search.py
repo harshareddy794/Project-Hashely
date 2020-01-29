461e5014b8b44763df727a9fed3b82fcd2c15fa5
Gaddam Harshavardhan <48166328+harshareddy794@users.noreply.github.com>
2020-01-29 21:50:58 +0530

    Update search.py

--- Search hash/search.py
+++ Search hash/search.py
@@ -1,16 +1,16 @@
 import xlrd
-def sha256(row,col,inp):
-    for i in range(0,row):
-        if(inp==sheet.cell_value(i,1)):
+def sha256(rows,cols,inp):
+    for row in range(0,rows):
+        if(inp==sheet.cell_value(row,1)):
             print("Found data in database.It's SHA256 hash:")
-            print(sheet.cell_value(i,0))
+            print(sheet.cell_value(row,0))
             break
     print("Hash not found in database")
-def md5(row,col,inp):
-    for i in range(0,row):
-        if(inp==sheet.cell_value(i,2)):
+def md5(rows,cols,inp):
+    for row in range(0,rows):
+        if(inp==sheet.cell_value(row,2)):
             print("Found data in database.It's MD5 hash:")
-            print(sheet.cell_value(i,0))
+            print(sheet.cell_value(row,0))
             break
     print("Hash not found in database")
 if __name__ == '__main__':
@@ -18,13 +18,13 @@ if __name__ == '__main__':
     inp=input("Enter input:")
     wb=xlrd.open_workbook(file)
     sheet = wb.sheet_by_index(0)
-    row=sheet.nrows
-    col=sheet.ncols
+    rows=sheet.nrows
+    cols=sheet.ncols
     if(len(inp)==64):
         print("Searching data in database")
-        sha256(row,col,inp)
+        sha256(rows,cols,inp)
     elif(len(inp)==32):
         print("Searching data in database")
-        md5(row,col,inp)
+        md5(rows,cols,inp)
     else:
         print("Invalid hash format")
